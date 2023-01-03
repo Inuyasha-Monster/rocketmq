49a51f31d9e046e0fa3d345ab1f52b310a64db1a
djl <djl.du@huolala.cn>
2023-01-03 09:40:06 +0800

    重新加深理解indexFile逻辑

--- store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
+++ store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
@@ -2085,7 +2085,7 @@ public class DefaultMessageStore implements MessageStore {
 
                             if (dispatchRequest.isSuccess()) {
                                 if (size > 0) {
-                                    // TODO: 2022/6/10 构建索引文件包括：consumerQueue + indexFile
+                                    // 触发构建索引文件包括：consumerQueue + indexFile
                                     DefaultMessageStore.this.doDispatch(dispatchRequest);
 
                                     if (BrokerRole.SLAVE != DefaultMessageStore.this.getMessageStoreConfig().getBrokerRole()
